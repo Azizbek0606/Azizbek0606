97ba1afae0f6b23add224f6f5de566b8f6f9a395
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2022-10-12 18:37:55 +0500

    index..html

---
+++ js/app.js
@@ -0,0 +1,25 @@
+// data types - malumot turlari 
+
+// 1- string : oddiy matn , belgi >> "" , '' ,  ``
+// 2- number : -1 , 1 , 2 ,1.3
+// 3- boolen : true / false 
+// 4- null : mavjud bo'lmagan qiymat 
+// 5- underfined : qiymati mavjud bo'lmagan ma'lumot
+// 6- objeky : obyekt
+// 7- bigint : katta butun son 
+// 8- infinity : cheksiz sonlar 
+
+// let str = 'matn';
+// let str2 = "text";
+// // ES6 _ `` >>> ${O'garuvchi nomi};
+// let main = `varible ${str}`; // varible = matn;
+
+
+//  null 
+// let n = null;
+// console.log(typeof n)
+
+// // undefined
+// let myVar
+// console.log(myVar) undefined
+ console.log(10 ** 308)
